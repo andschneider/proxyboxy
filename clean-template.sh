d219326dfc96f46136906899fc42214e2f994f1c
Andrew Schneider <aschne93@gmail.com>
2020-06-16 23:28:18 -0700

    Clean VM for ubuntu 20.04
    Using the cloud-init methodology isn't working for getting an IP from
    DHCP - the machine-id is still the same. So, bringing back the clean
    script.

---
+++ clean-template.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# cleans a freshly cloned VM template
+
+# change machine-id
+rm -f /var/lib/dbus/machine-id
+rm -f /etc/machine-id
+dbus-uuidgen --ensure=/etc/machine-id
+ln -s /etc/machine-id /var/lib/dbus/
+
+# reset SSH keys
+ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa -y
+ssh-keygen -f /etc/ssh/ssh_host_dsa_key -N '' -t dsa -y
+ssh-keygen -f /etc/ssh/ssh_host_ecdsa_key -N '' -t ecdsa -b 521 -y
+
